0e0c1c32b0becdb3223dda8a6e9104120950f934
Alexandre Ouellet <ouellet.alexandre@cegepvicto.ca>
2024-08-28 15:22:50 -0400

    Avancement cours 28 aout

---
+++ Assets/Scripts/PotFleur.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+/// <summary>
+/// Gère la physique des pots de fleurs
+/// </summary>
+public class PotFleur : MonoBehaviour
+{
+    /// <summary>
+    /// Le pot de fleur affiché au joueur
+    /// </summary>
+    [SerializeField]
+    private GameObject potInitial;
+
+    /// <summary>
+    /// Modèle de pot de fleur cassé
+    /// </summary>
+    [SerializeField]
+    private GameObject potCasse;
+
+    /// <summary>
+    /// Action du joueur casser
+    /// </summary>
+    public void OnCasser()
+    {
+        // S'il y a un pot, on le supprime
+        if (potInitial != null)
+        {
+            Destroy(potInitial);
+            GameObject nouvelObjet = Instantiate(potCasse, transform);      // Nouveau pot cassé
+        }
+    }
+}
